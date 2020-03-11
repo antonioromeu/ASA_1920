dee60a9e8b6478b8a8a0e3b85e8dc1747ddacbe1
marianacintrao <mariana.cintrao@gmail.com>
2020-03-11 19:41:41 +0000

    180 on mooshak

--- project1/main.cpp
+++ project1/main.cpp
@@ -6,13 +6,9 @@
 #define max(a, b) (a < b ? b : a)
 using namespace std;
 
-int *grades, *disc, *low;
-bool *stackMember;
-int time_ = 0;
-stack<int> *st = new stack<int>();
-
 class Graph {
-    int _students, _relationships;
+    int _students, _relationships, *_grades;
+    bool *_disc;
     vector<int> *_adj;
 
     public:
@@ -20,65 +16,47 @@ class Graph {
         _students = students;
         _relationships = relationships;
         _adj = new vector<int>[_students];
+        _grades = new int[students];
+        _disc = new bool[students];
     }
 
     void setStudents(int students) { _students = students; }
     void setRelationships(int relationships) { _relationships = relationships; }
+    void setGrade(int index, int grade) { _grades[index] = grade; }
+    void setDisc(int index, bool b) { _disc[index] = b; }
     vector<int> getAdjList(int index) { return _adj[index]; }
     void setAdj(int index, int adj) { _adj[index].push_back(adj); }
     void freeG() { delete[] _adj; }
 
-    void SCCaux(int vertix) {
-        disc[vertix] = low[vertix] = ++time_;
-        stackMember[vertix] = true;
-        st->push(vertix);
+    void DFSaux(int vertix) {
+        _disc[vertix] = true;
         for (int i = 0; i < (int) getAdjList(vertix).size(); i++) {
             int adjacent = getAdjList(vertix)[i];
-            //grades[vertix] = max(grades[vertix], grades[adjacent]);
-            if (disc[adjacent] == NIL) {
-                SCCaux(adjacent);
-                low[vertix] = min(low[vertix], low[adjacent]);
-                //grades[vertix] = max(grades[vertix], grades[adjacent]);
-            }
-            else if (stackMember[adjacent]) {
-                low[vertix] = min(low[vertix], disc[adjacent]);
-            }
-            grades[vertix] = max(grades[vertix], grades[adjacent]);
-        }
-        if (low[vertix] == disc[vertix]) { 
-            int v;
-            while (st->top() != vertix) { 
-                v = (int) st->top(); 
-                stackMember[v] = false;
-                st->pop();
-            } 
-            v = (int) st->top(); 
-            stackMember[v] = false;
-            st->pop();
+            if (!_disc[adjacent])
+                DFSaux(adjacent);
+            _grades[vertix] = max(_grades[vertix], _grades[adjacent]);
         }
     }
 
-    void SCC() {
-        for (int i = 0; i < _students; i++) {
-            if (disc[i] == NIL)
-                SCCaux(i);
-        }
+    void DFS() {
+        for (int i = 0; i < _students; i++)
+            if (!_disc[i])
+                DFSaux(i);
+    }
+
+    void printGrades() {
+        for (int i = 0; i < _students; i++)
+            cout << _grades[i] << endl;
     }
 };
 
 Graph* graphInit(int students, int relationships) {
     int grade, from, to;
     Graph* graph = new Graph(students, relationships);
-    grades = new int[students];
-    disc = new int[students];
-    low = new int[students];
-    stackMember = new bool[students];
     for (int i = 0; i < students; i++) {
         scanf("%d", &grade);
-        grades[i] = grade;
-        low[i] = NIL;
-        disc[i] = NIL;
-        stackMember[i] = false;
+        graph->setGrade(i, grade);
+        graph->setDisc(i, false);
     }
     for (int i = 0; i < relationships; i++) {
         scanf("%d %d", &from, &to);
@@ -87,13 +65,7 @@ Graph* graphInit(int students, int relationships) {
     return graph;
 }
 
-void free(Graph *graph) {
-  delete st;
-  delete[] disc;
-  delete[] low;
-  delete[] stackMember;
-  graph->freeG();
-}
+void free(Graph *graph) { graph->freeG(); }
 
 int main() {
     int students, relationships;
@@ -106,9 +78,17 @@ int main() {
         exit(EXIT_FAILURE);
     }
     Graph *g = graphInit(students, relationships);
-    g->SCC();
+    g->DFS();
     for (int i = 0; i < students; i++)
-        cout << grades[i] << endl;
+        g->setDisc(i, false);
+    g->DFS();
+    for (int i = 0; i < students; i++)
+        g->setDisc(i, false);
+    g->DFS();
+    for (int i = 0; i < students; i++)
+        g->setDisc(i, false);
+    g->DFS();
+    g->printGrades();
     free(g);
     return 0;
 }
