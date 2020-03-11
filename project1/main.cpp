32cdb93e5d8c58bcfb8567442eb473e761b9e82e
marianacintrao <mariana.cintrao@gmail.com>
2020-03-11 20:45:21 +0000

    final 1

--- project1/main.cpp
+++ project1/main.cpp
@@ -7,26 +7,30 @@
 using namespace std;
 
 class Graph {
-    int _students, _relationships, *_grades;
+    int _students, _relationships;
     bool *_disc;
+    vector<int> _grades;
     vector<int> *_adj;
 
     public:
     Graph(int students, int relationships) {
         _students = students;
         _relationships = relationships;
-        _adj = new vector<int>[_students];
-        _grades = new int[students];
+        _adj = new vector<int>[students];
+        _grades = *(new vector<int>[students]);
         _disc = new bool[students];
     }
 
     void setStudents(int students) { _students = students; }
     void setRelationships(int relationships) { _relationships = relationships; }
-    void setGrade(int index, int grade) { _grades[index] = grade; }
+    void setGrade(int grade) { _grades.push_back(grade); }
     void setDisc(int index, bool b) { _disc[index] = b; }
-    vector<int> getAdjList(int index) { return _adj[index]; }
     void setAdj(int index, int adj) { _adj[index].push_back(adj); }
-    void freeG() { delete[] _adj; }
+    
+    vector<int> getAdjList(int index) { return _adj[index]; }
+    vector<int> getGrades() { return _grades; }
+    
+    void freeG() { delete[] _adj; delete[] _disc; }
 
     void DFSaux(int vertix) {
         _disc[vertix] = true;
@@ -38,10 +42,14 @@ class Graph {
         }
     }
 
-    void DFS() {
+    vector<int> DFS() {
+        for (int i = 0; i < _students; i++)
+            setDisc(i, false);
+
         for (int i = 0; i < _students; i++)
             if (!_disc[i])
                 DFSaux(i);
+        return _grades;
     }
 
     void printGrades() {
@@ -53,15 +61,17 @@ class Graph {
 Graph* graphInit(int students, int relationships) {
     int grade, from, to;
     Graph* graph = new Graph(students, relationships);
+    
     for (int i = 0; i < students; i++) {
         scanf("%d", &grade);
-        graph->setGrade(i, grade);
-        graph->setDisc(i, false);
+        graph->setGrade(grade);
     }
+
     for (int i = 0; i < relationships; i++) {
         scanf("%d %d", &from, &to);
         graph->setAdj(from-1, to-1);
     }
+
     return graph;
 }
 
@@ -78,16 +88,13 @@ int main() {
         exit(EXIT_FAILURE);
     }
     Graph *g = graphInit(students, relationships);
-    g->DFS();
-    for (int i = 0; i < students; i++)
-        g->setDisc(i, false);
-    g->DFS();
-    for (int i = 0; i < students; i++)
-        g->setDisc(i, false);
-    g->DFS();
-    for (int i = 0; i < students; i++)
-        g->setDisc(i, false);
-    g->DFS();
+
+    vector<int> grades = *(new vector<int>[students]);
+    vector<int> newGrades = *(new vector<int>[students]);
+    grades = g->getGrades();
+    while (grades != (newGrades = g->DFS())) {
+        grades = newGrades;
+    }
     g->printGrades();
     free(g);
     return 0;
