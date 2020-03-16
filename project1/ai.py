2727e5dc2d3ead5d9ea5f68da417cf2657cc7383
marianacintrao <mariana.cintrao@gmail.com>
2020-03-16 19:18:47 +0000

    updates

--- project1/ai.py
+++ project1/ai.py
@@ -1,20 +1,25 @@
 import random
 import os
-maxStudents = 50
-students = 0
-adj = 0
-students = int(random.random()*maxStudents)
+#maxStudents = 1000000
+students = 10000
+#students = int(random.random()*maxStudents)
 grades = []
 adjList = []
-adj = int(random.random()*maxStudents)
+adj = 501000
 
-print(str(students)+','+str(adj))
+#print(str(students)+','+str(adj))
+s = ''
 for i in range(students):
-    print(random.randint(1, 21))
+    s += str(random.randint(1, 20)) + '\n'
+s = s[:-1]
 
 for i in range(adj):
-    t = tuple((random.randint(0, students), random.randint(0, students)))
+    t = tuple((random.randint(1, students), random.randint(1, students)))
     adjList += [t]
 adjList = list(set(adjList))
+
+print(str(students)+','+str(len(adjList)))
+print(s)
 for i in range(len(adjList)):
     print(adjList[i][0], adjList[i][1])
+
--- project1/main.cpp
+++ project1/main.cpp
@@ -1,61 +1,52 @@
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <chrono>
 #define NIL -1
 #define max(a, b) (a < b ? b : a)
 using namespace std;
 
 class Graph {
-    int _students, _relationships, *_grades;
+    int _students, _relationships;
     bool *_disc;
+    vector<int> _grades;
     vector<int> *_adj;
 
     public:
     Graph(int students, int relationships) {
         _students = students;
         _relationships = relationships;
-        _adj = new vector<int>[_students];
-        _grades = new int[students];
+        _adj = new vector<int>[students];
+        _grades = *(new vector<int>[students]);
         _disc = new bool[students];
     }
 
-    void setGrade(int index, int grade) { _grades[index] = grade; }
+    void setGrade(int grade) { _grades.push_back(grade); }
     void setDisc(int index, bool b) { _disc[index] = b; }
-    vector<int> getAdjList(int index) { return _adj[index]; }
     void setAdj(int index, int adj) { _adj[index].push_back(adj); }
-    void freeG() { delete[] _adj; }
+    
+    vector<int> getAdjList(int index) { return _adj[index]; }
+    vector<int> getGrades() { return _grades; }
+    
+    void freeG() { delete[] _adj; delete[] _disc; }
 
-    void propagateDiscs(int vertex) {
-        _disc[vertex] = false;
-        int grade = _grades[vertex];
-        for (int i = 0; i < (int) getAdjList(vertex).size(); i++) {
-            int adjacent = getAdjList(vertex)[i];
-            if (_grades[adjacent] < grade)
-                propagateDiscs(adjacent);
-            _disc[i] = false;
+    void DFSaux(int vertix) {
+        _disc[vertix] = true;
+        for (int i = 0; i < (int) getAdjList(vertix).size(); i++) {
+            int adjacent = getAdjList(vertix)[i];
+            if (!_disc[adjacent])
+                DFSaux(adjacent);
+            _grades[vertix] = max(_grades[vertix], _grades[adjacent]);
         }
     }
 
-    void DFSaux(int vertex) {
-        _disc[vertex] = true;
-        for (int i = 0; i < (int) getAdjList(vertex).size(); i++) {
-            while (true) {
-                int grade = _grades[vertex];
-                int adjacent = getAdjList(vertex)[i];
-                if (!_disc[adjacent])
-                    DFSaux(adjacent);
-                _grades[vertex] = max(_grades[vertex], _grades[adjacent]);
+    vector<int> DFS() {
+        for (int i = 0; i < _students; i++)
+            setDisc(i, false);
 
-                if (_grades[vertex] == grade) break;
-                else propagateDiscs(vertex);
-            }
-        }
-    }
-
-    void DFS() {
         for (int i = 0; i < _students; i++)
             if (!_disc[i])
                 DFSaux(i);
+        return _grades;
     }
 
     void printGrades() {
@@ -67,15 +58,17 @@ class Graph {
 Graph* graphInit(int students, int relationships) {
     int grade, from, to;
     Graph* graph = new Graph(students, relationships);
+    
     for (int i = 0; i < students; i++) {
         scanf("%d", &grade);
-        graph->setGrade(i, grade);
-        graph->setDisc(i, false);
+        graph->setGrade(grade);
     }
+
     for (int i = 0; i < relationships; i++) {
         scanf("%d %d", &from, &to);
         graph->setAdj(from-1, to-1);
     }
+
     return graph;
 }
 
@@ -83,6 +76,9 @@ void free(Graph *graph) { graph->freeG(); }
 
 int main() {
     int students, relationships;
+
+    auto start = chrono::steady_clock::now();
+
     if (!scanf("%d,%d", &students, &relationships)) {
         printf("Error on scanf\n");
         exit(EXIT_FAILURE);
@@ -92,8 +88,23 @@ int main() {
         exit(EXIT_FAILURE);
     }
     Graph *g = graphInit(students, relationships);
-    g->DFS();
+
+    vector<int> grades = *(new vector<int>[students]);
+    vector<int> newGrades = *(new vector<int>[students]);
+    grades = g->getGrades();
+    while (grades != (newGrades = g->DFS())) {
+        grades = newGrades;
+    }
+
     g->printGrades();
     free(g);
+
+    auto end = chrono::steady_clock::now(); //////////////////////////
+    auto diff = end - start;
+    
+    cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
+
+
+    printf("%d %d\n", students, relationships);
     return 0;
 }
